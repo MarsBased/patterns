1c23d2df5aa227cbed17d4e20237541ebfcd17b9
Xavier Redó <xavier.redo@marsbased.com>
2019-07-25 11:36:53 +0200

    Add the command pattern as a MarsBased pattern

---
+++ rails/commands-pattern/app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+
+  def create
+    authorize User
+
+    Users::Create.new.call(user_params).then do |result|
+      if result.ok
+        redirect_to dashboard_path
+      else
+        @user = result.user
+        flash.now[:error] = t('.error')
+        render :new
+      end
+    end
+  end
+
+  # There is no need to use a command here, since the action to perform is simple and
+  # does not have side effects. Adding a command here would increase complexity.
+  def update
+    @user = authorize User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to dashboard_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params; end
+
+end
